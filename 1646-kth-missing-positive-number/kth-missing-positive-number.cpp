6ef84a5943375a0b9b17b8fc41d1f6c1f92c1987
Tushar Jain <jaint0910@gmail.com>
2026-07-25 00:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (29.67%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
+
+
+
+        //brute force  agar merko array wala number k se chota ya equal hai tho k++ as we find bigger then it we break
         for( int i = 0 ; i < arr.size() ; i++ ){
             if(arr[i]<=k) k++;
             else break;
