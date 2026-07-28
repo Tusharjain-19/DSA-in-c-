a18896c8a77a393182d7327f79f9446ad0c40813
Tushar Jain <jaint0910@gmail.com>
2026-07-29 00:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (79.69%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -5,11 +5,13 @@ public:
         int m = matrix[0].size();
         int ans = -1;
         for(int i =0 ;i<n;i++){
+            //brute   O(n*m)
             // for(int j = 0 ;j<matrix[i].size();j++){
             //     if(matrix[i][j]==target){
             //         ans = 1;
             //     }
             // }
+            // optimal O(n*log m)
             int low = 0 , high = m-1;
             while(low<=high){
                 int mid = (low+high)/2;
@@ -25,7 +27,7 @@ public:
                 }
             }
         }
-        if (ans ==1) return true;
+        if (ans == 1) return true;
 
         return false;   
     }
