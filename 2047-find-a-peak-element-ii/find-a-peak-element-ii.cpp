ab6da53ed65c36bac59c26756505071486d1767f
Tushar Jain <jaint0910@gmail.com>
2026-07-30 00:09:41 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (15.86%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        //it say we take mid column then check max elemt in that column and find it's row on which the max elemnt with column mid and then we check with its right and left because as it is max in cloumn it is already peak so no up down check need
+        //why max soch lala max element ki probility jayda hai max niklne ki as max se pata lg gaya column mai tho max hai vo 
         int n = mat.size();
         int m = mat[0].size();
         int low = 0 , high = m-1;   
@@ -11,8 +13,11 @@ public:
                 if(mat[i][mid]>mat[max_row][mid])
                 max_row = i;
             }
+
             int left = (mid > 0)? mat[max_row][mid-1] : -1;
             int right = (mid < m-1)? mat[max_row][mid+1]:-1;
+
+
             if(mat[max_row][mid]>left && mat[max_row][mid]>right){
                 return {max_row,mid};
             }
