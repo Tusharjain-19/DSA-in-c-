26658c054926c3372db158dd148d97fcd4147d77
Tushar Jain <jaint0910@gmail.com>
2026-07-31 23:00:09 +0530

    Time: 2311 ms (5.23%) | Memory: 23.7 MB (68.09%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> list;
+        
+        for (int i = 0; i < n; i++) {
+            // Check if nums[i] is already in our list
+            bool alreadyExists = false;
+            for (int val : list) {
+                if (val == nums[i]) {
+                    alreadyExists = true;
+                    break;
+                }
+            }
+            // If it's not already in the list, count its occurrences
+            if (!alreadyExists) {
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    if (nums[j] == nums[i]) {
+                        count++;
+                    }
+                }
+                if (count > n / 3) {
+                    list.push_back(nums[i]);
+                }
+            }
+        } 
+        return list;    
+    }
+};
