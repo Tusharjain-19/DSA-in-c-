e66f177bc4a415aa3b47f36431b37509d610d940
Tushar Jain <jaint0910@gmail.com>
2026-08-01 00:00:32 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (46.43%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        //use of map 
+        //vector<int>list;
+        // map<int,int>mpp;
+        // int n = nums.size();
+        // int mm = n/3+1;
+        // for( int i=0; i<n ;i++){
+        //     mpp[nums[i]]++;
+        //     if(mpp[nums[i]]==mm){
+        //         list.push_back(nums[i]);
+        //     }
+        // }  
+        // return list; 
+
+        int cnt1 = 0 , cnt2 = 0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
         int n = nums.size();
+        for(int i=0 ;i<n;i++){
+            if(cnt1==0 && el2!=nums[i]){
+                cnt1 = 1;
+                el1 = nums[i]; 
+            }
+            else if( cnt2 == 0 && el1 != nums[i]){
+                cnt2 = 1;
+                el2 =nums[i];
+            }
+            else if (el1 == nums[i]) cnt1++;
+            else if (el2 == nums[i]) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        //manual checking for both number 
         vector<int> list;
-        
-        for (int i = 0; i < n; i++) {
-            // Check if nums[i] is already in our list
-            int alreadyExists = -1;
-            for (int val : list) {
-                if (val == nums[i]) {
-                    alreadyExists = 1;
-                    break;
-                }
-            }
-            // If it's not already in the list, count its occurrences
-            if (alreadyExists == -1) {
-                int count = 0;
-                for (int j = 0; j < n; j++) {
-                    if (nums[j] == nums[i]) {
-                        count++;
-                    }
-                }
-                if (count > n / 3) {
-                    list.push_back(nums[i]);
-                }
-            }
-        } 
-        return list;    
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i =0 ;i<n;i++){
+            if(el1==nums[i])cnt1++;
+            if(el2==nums[i])cnt2++;
+        }
+        int mini = (int)n/3+1;
+        if(cnt1>=mini) list.push_back(el1);
+        if(cnt2>=mini) list.push_back(el2);
+        sort(list.begin(),list.end());
+        return list;
     }
 };
