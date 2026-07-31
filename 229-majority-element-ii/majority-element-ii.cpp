a05fab0c524e8a44034088f77ff64de472b80d86
Tushar Jain <jaint0910@gmail.com>
2026-07-31 23:02:07 +0530

    Time: 2313 ms (5.23%) | Memory: 23.8 MB (46.43%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -6,15 +6,15 @@ public:
         
         for (int i = 0; i < n; i++) {
             // Check if nums[i] is already in our list
-            bool alreadyExists = false;
+            int alreadyExists = -1;
             for (int val : list) {
                 if (val == nums[i]) {
-                    alreadyExists = true;
+                    alreadyExists = 1;
                     break;
                 }
             }
             // If it's not already in the list, count its occurrences
-            if (alreadyExists == false) {
+            if (alreadyExists == -1) {
                 int count = 0;
                 for (int j = 0; j < n; j++) {
                     if (nums[j] == nums[i]) {
